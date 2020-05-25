43194c2350a44b5d6aeefaf1ea25d730efe652a4
earogov <earogov@gmail.com>
2020-05-25 09:41:16 +0300

    Populate users db for prod

--- src/main/resources/db/changelog/populateDB-test.sql
+++ src/main/resources/db/changelog/populateDB-test.sql
@@ -1,21 +1,3 @@
-INSERT INTO users VALUES (
-'1',
-'admin',
-'{bcrypt}$2a$10$T8y5atzjwrUNfTwvw4.ugOsIqIb3UZncogi8vp5c5RcgQm/8EOB4O',
-'',
-'',
-'admin@server.com',
-'ROLE_ADMIN,ROLE_GUEST');
-
-INSERT INTO users VALUES (
-'2',
-'guest',
-'{bcrypt}$2a$10$DioCIo6RqkrAc6FAAnJ27eOQwDQk2a2GPL4RFECtFSPgCT/hP4jd2',
-'',
-'',
-'guest@server.com',
-'ROLE_GUEST');
-
 INSERT INTO authors VALUES ('1', 'Peter Watts');
 INSERT INTO authors VALUES ('2', 'Greg Egan');
 
